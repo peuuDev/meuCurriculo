3b554143edb71e2d4c784146973cc93b6eb68287
peuDev_ <peuramos1@gmail.com>
2026-09-16 09:34:10 -0300

    meuCurriculo v4 - adição do botão Ver mais e suas manipulaçoes de estado

--- script.js
+++ script.js
@@ -10,12 +10,53 @@ window.addEventListener("scroll", () =>{
     }
 })
 
-//Função onClick do botão
-btnTopo.addEventListener("click", () =>{
 
+//Função onClick do botão 
+btnTopo.addEventListener("click", () =>{
+    
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
+    
+});
+
+//Funçoes btnSobre
+const btnSobre = document.querySelector("#btnSobre");
+
+let estado = 1;
+
+btnSobre.addEventListener("mouseenter", () => {
+
+    if (estado === 1) {
+        estado = 2;
+        btnSobre.classList.add("estado2");
+    }
+
+});
+
+btnSobre.addEventListener("mouseleave", () => {
+    if(estado === 2){
+        estado = 1;
+        btnSobre.classList.remove("estado2");
+    }
+})
+
+btnSobre.addEventListener("click", () => {
+
+    if (estado === 2) {
+
+        estado = 3;
+
+        btnSobre.classList.remove("estado2");
+        btnSobre.classList.add("estado3");
+
+    } else if (estado === 3) {
+
+        estado = 1;
+
+        btnSobre.classList.remove("estado3");
+
+    }
 
 });
